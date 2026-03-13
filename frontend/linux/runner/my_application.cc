88e3713fc3531a094a43d443d4b19e7c2d2b0e24
Metin OSMAN <4737770+MrCitron@users.noreply.github.com>
2026-03-13 01:46:36 +0100

    feat(branding): Set application icon for Linux and bundle asset

--- frontend/linux/runner/my_application.cc
+++ frontend/linux/runner/my_application.cc
@@ -54,6 +54,12 @@ static void my_application_activate(GApplication* application) {
 
   gtk_window_set_default_size(window, 1280, 720);
 
+  // Set application icon
+  g_autoptr(GdkPixbuf) icon = gdk_pixbuf_new_from_file("data/flutter_assets/assets/icon.png", nullptr);
+  if (icon != nullptr) {
+    gtk_window_set_icon(window, icon);
+  }
+
   g_autoptr(FlDartProject) project = fl_dart_project_new();
   fl_dart_project_set_dart_entrypoint_arguments(
       project, self->dart_entrypoint_arguments);
